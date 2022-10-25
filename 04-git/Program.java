cc3c3ba9d313fca1dff223536a68ae0161d63807
unknown <inf150185@lab-45-14.cs.put.poznan.pl>
2022-10-25 08:20:54 +0200

    Add Program.java

---
+++ 04-git/Program.java
@@ -0,0 +1,13 @@
+public class Class_Example 
+{
+    public Class_Example() 
+    {
+        String value = "Test";// Local Variable inside a Constructor
+    }
+
+    public void disp() 
+    {
+        String name = "JavaInterviewPoint";// Local variable inside method
+        System.out.println("Welcome " + name + "!!!");
+    }
+}
