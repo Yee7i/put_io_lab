bbde829cadf8d3183e428a7888c66aef92e22d2e
Adrian Tomicki <adrek07@gmail.com>
2022-10-31 16:22:52 +0100

    3 changes to Program

--- 04-git/Program.java
+++ 04-git/Program.java
@@ -1,13 +1,15 @@
-public class Class_Example 
+public class Class_Example // nastepna zmiana do amenda
 {
-    public Class_Example() 
+    public Class_Example() // fragment 2
     {
         String value = "Test 2";// Local Variable inside a Constructor
     }
 
+    // fragment 1
+
     public void disp() 
     {
-        String name = "JavaInterviewPoint2";// Local variable inside method
+        String name = "JavaInterviewPoint";// Local variable inside method fragment 3
 
         System.out.println("Welcome " + name); // zmiana 1
     }
